ac03f7abbd4239959140b65c34443fd402b244db
--replace-all <jndayele@gmail.com>
2023-07-14 12:43:52 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -20,9 +20,9 @@ int main(void)
 	for (d = '0'; d <= '9'; d++)
 	putchar(d);
 
-	for (low = 'a' low <= 'f'; low+)
+	for (low = 'a'; low <= 'f'; low++)
 	putchar(low);
-	purchar('\n');
+	putchar('\n');
 
 	return (0);
 
